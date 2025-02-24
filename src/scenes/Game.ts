356823157825629bfa73c6f590e44c3c9b07f496
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-02-24 15:46:45 -0500

    working on the main game scene

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -10,29 +10,17 @@ export class Game extends Scene {
     }
 
     create() {
+        // todo
+        // game start
+        // Words faling
+        // Level on the top right
+        // Score under the level
+        // City health under the level
+
         this.camera = this.cameras.main;
         this.camera.setBackgroundColor(0x00ff00);
 
         this.background = this.add.image(512, 384, "background");
         this.background.setAlpha(0.5);
-
-        this.msg_text = this.add.text(
-            512,
-            384,
-            "Make something fun!\nand share it with us:\nsupport@phaser.io",
-            {
-                fontFamily: "Arial Black",
-                fontSize: 38,
-                color: "#ffffff",
-                stroke: "#000000",
-                strokeThickness: 8,
-                align: "center",
-            },
-        );
-        this.msg_text.setOrigin(0.5);
-
-        this.input.once("pointerdown", () => {
-            this.scene.start("GameOver");
-        });
     }
 }

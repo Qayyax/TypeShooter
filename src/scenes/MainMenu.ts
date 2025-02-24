d9461dcc7f02563b4728982b677a4770557c2935
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-02-24 15:38:03 -0500

    first scene

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -1,32 +1,54 @@
-import { Scene, GameObjects } from 'phaser';
+import { Scene, GameObjects } from "phaser";
 
-export class MainMenu extends Scene
-{
+export class MainMenu extends Scene {
     background: GameObjects.Image;
     logo: GameObjects.Image;
     title: GameObjects.Text;
 
-    constructor ()
-    {
-        super('MainMenu');
+    constructor() {
+        super("MainMenu");
     }
 
-    create ()
-    {
-        this.background = this.add.image(512, 384, 'background');
+    create() {
+        // todo
+        // Change logo
+        // Add music maybe
+        // Fadeaway animation for the speed
+        //
+        this.background = this.add.image(512, 384, "background");
 
-        this.logo = this.add.image(512, 300, 'logo');
+        this.logo = this.add.image(512, 300, "logo");
 
-        this.title = this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+        this.title = this.add
+            .text(512, 460, "Play", {
+                fontFamily: "Arial Black",
+                fontSize: 38,
+                color: "#ffffff",
+                stroke: "#000000",
+                strokeThickness: 8,
+                align: "center",
+            })
+            .setOrigin(0.5)
+            .setInteractive();
 
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('Game');
-
+        this.title.on("pointerdown", () => {
+            this.changeScene("Game");
         });
+
+        this.input.keyboard!.on("keydown-ENTER", () => {
+            this.changeScene("Game");
+        });
+
+        // Adding style on click
+        this.title.on("pointerover", () => {
+            this.title.setStyle({ color: "#842929" });
+        });
+        this.title.on("pointerout", () => {
+            this.title.setStyle({ color: "#ffffff" });
+        });
+    }
+
+    changeScene(scene: string) {
+        this.scene.start(scene);
     }
 }

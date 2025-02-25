1847b110fc6217a3816c0f5010b183efb63516b4
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-02-25 13:01:18 -0500

    game scene

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,9 +1,21 @@
 import { Scene } from "phaser";
 
+// random words library
+// https://www.npmjs.com/package/random-words
+
 export class Game extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
-    msg_text: Phaser.GameObjects.Text;
+    sky: Phaser.GameObjects.Graphics;
+    healthBar: Phaser.GameObjects.Graphics;
+    cityHealth: number = 100;
+    maxCityHealth: number = 100;
+    healthBarWidth: number = 100;
+    score: number = 0;
+    level: number = 0;
+    scoreText: Phaser.GameObjects.Text;
+    levelText: Phaser.GameObjects.Text;
+    cityText: Phaser.GameObjects.Text;
 
     constructor() {
         super("Game");
@@ -14,7 +26,7 @@ export class Game extends Scene {
         // game start
         // Words faling
         // Level on the top right
-        // Score under the level
+        // cityHealth under the level
         // City health under the level
 
         this.camera = this.cameras.main;
@@ -22,5 +34,23 @@ export class Game extends Scene {
 
         this.background = this.add.image(512, 384, "background");
         this.background.setAlpha(0.5);
+
+        // The top sky where the words fall from
+        this.sky = this.add.graphics();
+        this.sky.fillStyle(0x00000, 0.8);
+        this.sky.fillRect(0, 0, 1024, 70);
+        this.sky.setDepth(50);
+
+        const barBackground = this.add.graphics();
+        barBackground.fillStyle(0x444444, 1);
+        // todo - add city health text in front
+        barBackground.fillRoundedRect(900, 20, this.healthBarWidth, 20, 10);
+
+        this.healthBar = this.add.graphics();
+    }
+
+    updateCityHealthBar() {
+        // todo - update health bar when words fall on floor
+        // this.cityHealthBar.clear();
     }
 }
